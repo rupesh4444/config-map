094c83f9de9a05d190974fd309d3416ef20ce82f
rupesh4444 <kumarrupesh.kumar4444@gmail.com>
2022-06-02 18:31:50 +0530

    env variable working

--- src/main/java/com/config/configmap/controller/AppController.java
+++ src/main/java/com/config/configmap/controller/AppController.java
@@ -1,13 +1,9 @@
 package com.config.configmap.controller;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@Configuration
-@ConfigurationProperties(prefix = "mssgs")
 @RestController
 public class AppController {
 
